9cd93638077725f52638c44902f93107ee8648f2
Nichelle <nreynolds1202@gmail.com>
2022-11-22 13:27:33 -0500

    index.js updates

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ export const birdData = [
         count: 0,
         sizeShape: "Plump-bodied and long-tailed, with short legs, small bill, and a head that looks particularly small in comparison to the body. The long, pointed tail is unique among North American doves.",
         colorPattern:"Mourning Doves often match their open-country surroundings. They’re delicate brown to buffy-tan overall, with black spots on the wings and black-bordered white tips to the tail feathers.",
-        uuid:""
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee'
     },
     {
         species: "Northern Cardinal",
@@ -13,7 +13,7 @@ export const birdData = [
         count: 0,
         sizeShape: "The Northern Cardinal is a fairly large, long-tailed songbird with a short, very thick bill and a prominent crest. Cardinals often sit with a hunched-over posture and with the tail pointed straight down.",
         colorPattern:"Male cardinals are brilliant red all over, with a reddish bill and black face immediately around the bill. Females are pale brown overall with warm reddish tinges in the wings, tail, and crest. They have the same black face and red-orange bill.",
-        uuid:""
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2'
     },
     {
         species: "Blue Jay",
@@ -21,6 +21,6 @@ export const birdData = [
         count: 0,
         sizeShape: "Large crested songbird with broad, rounded tail. Blue Jays are smaller than crows, larger than robins.",
         colorPattern:"White or light gray underneath, various shades of blue, black, and white above.",
-        uuid:""
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45'
     }
 ]
--- index.js
+++ index.js
@@ -1,21 +1,60 @@
 import { birdData } from './data.js'
 
+document.addEventListener('click', function(e){
+    if(e.target.dataset.plus){
+        handlePlusClick(e.target.dataset.plus)
+    } else if (e.target.dataset.minus){
+        handleMinusClick(e.target.dataset.minus)
+    } else if (e.target.dataset.moreInfo){
+        handleMoreInfoClick(e.target.dataset.moreInfo)
+    }
+})
+
+function handlePlusClick(birdId){
+    const targetBirdObj = birdData.filter(function(bird){
+        return bird.uuid === birdId
+    }) [0]
+    targetBirdObj.count++
+    render()
+}
+
+function handleMinusClick(birdId){
+    const targetBirdObj = birdData.filter(function(bird){
+        return bird.uuid === birdId
+    }) [0]
+    targetBirdObj.count--
+    render()
+}
+
+function handleMoreInfoClick(infoId){
+    // const targetBirdObj = birdData.filter(function(bird){
+    //     return bird.uuid === birdId
+    // }) [0]
+    document.getElementById(`info-text-${infoId}`).classList.toggle('collapsed')
+    // console.log('test')
+}
+
 function getBirdHtml(){
     let  birdHtml = ""
     birdData.forEach(function(bird){
         birdHtml += `
-                <div>
-                    <img src= ${bird.image} class="">
+                <div class="main-panel">
+                    <div>
+                        <img src= ${bird.image} class="bird-pic">
+                    </div>
+                    <div>
+                        <h3 class="species-text">${bird.species}</h3>
+                        <button data-moreInfo="${bird.uuid}">More Info</button>
+                    </div>
+                    <div>
+                        <button data-plus="${bird.uuid}">+</button>
+                        <p id="sightings-num" >${bird.count}</p>
+                        <button data-minus="${bird.uuid}">-</button>
+                    </div>
                 </div>
-                <div>
-                    <h3 class="species-text">${bird.species}</h3>
+                <div id="info-text-${bird.uuid}" class="collapsed" >
                     <p class="size-shape">Size and Shape: ${bird.sizeShape}</p>
                     <p class="color-pattern">Color and Pattern: ${bird.colorPattern}</p>
-                </div>
-                <div>
-                    <button>+</button>
-                    <p>0</p>
-                    <button>-</button>
                 </div>
         `
     })
@@ -27,74 +66,3 @@ function render(){
 }
 
 render()
-
-
-
-// let numElBrown = document.getElementById("num-el-brown")
-// let numElOrange = document.getElementById("num-el-orange")
-// let numElDove = document.getElementById("num-el-dove")
-
-// let brownBtn = document.getElementById("brown-btn")
-// let orangeBtn = document.getElementById("orange-btn")
-// let doveBtn = document.getElementById("dove-btn")
-// let resetBtn = document.getElementById("reset-btn")
-
-// let totalNum = document.getElementById("total-num")
-// let totalBtn = document.getElementById("total-btn")
-
-// let brownCount = 0
-// let orangeCount = 0
-// let doveCount = 0
-
-
-// brownBtn.addEventListener("click", function() {
-//     brownCount+= 1
-//     numElBrown.textContent = brownCount
-// })
-
-// orangeBtn.addEventListener("click", function() {
-//     orangeCount+= 1
-//     numElOrange.textContent = orangeCount
-// })
-
-// doveBtn.addEventListener("click", function() {
-//     doveCount+= 1
-//     numElDove.textContent = doveCount
-// })
-
-// totalBtn.addEventListener("click", function() {
-//     if (brownCount === 0 && orangeCount === 0) {
-//         totalNum.textContent = `Today you spotted ${numElDove.textContent} Mourning doves!`
-//     }   else if (orangeCount === 0 && doveCount === 0) {
-//         totalNum.textContent = `Today you spotted ${numElBrown.textContent} House Sparrows!`
-//     }   else if (brownCount === 0 && doveCount === 0) {
-//         totalNum.textContent = `Today you spotted ${numElOrange.textContent} House Finches!`
-//     }   else if (brownCount === 0) {
-//         totalNum.textContent = `Today you spotted ${numElOrange.textContent} House Finches and ${numElDove.textContent} Mourning doves!`
-//     }   else if (orangeCount === 0) {
-//         totalNum.textContent = `Today you spotted ${numElBrown.textContent} House Sparrows and ${numElDove.textContent} Mourning doves!`
-//     }   else if (doveCount === 0) {
-//         totalNum.textContent = `Today you spotted ${numElBrown.textContent} House Sparrows and ${numElOrange.textContent} Orange Finches!`
-//     }   else {
-//         totalNum.textContent = `Today you spotted ${numElBrown.textContent} House Sparrows, ${numElOrange.textContent} House Finches, and ${numElDove.textContent} Mourning doves!`
-//     }
-// })
-
-// resetBtn.addEventListener("click", function() {
-//     brownCount = 0
-//     orangeCount = 0
-//     doveCount = 0
-//     numElBrown.textContent = 0
-//     numElOrange.textContent = 0
-//     numElDove.textContent = 0
-//     totalNum.textContent = ""
-    
-// })
-
-// Think of making birds dynamic. Storing in variables birdOne, birdTwo, birdThree and passing in bird species as Arguments. In case present birds change
-
-// Figure out copy button
-
-// Create an array that suggest other animals to spot
-
-//Add warning modal when resetting numbers
